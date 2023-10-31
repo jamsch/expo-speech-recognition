c746d4ff759ff2886e6e3b2047af3ce0ed6288c2
jamsch <jamsch@users.noreply.github.com>
2023-10-31 23:05:34 +1300

    feat(config-plugin): configure iOS permissions

--- config-plugin.js
+++ config-plugin.js
@@ -1,23 +1,15 @@
-const { withAndroidManifest } = require("expo/config-plugins");
+const {
+  withAndroidManifest,
+  AndroidConfig,
+  withPlugins,
+  createRunOncePlugin,
+} = require("expo/config-plugins");
 
-/** Appends Android 11 Package visibility filtering to AndroidManifest */
-const withExpoSpeechRecognition /*: ConfigPlugin*/ = (
-  config /*: ExpoConfig*/,
-) => {
+/**
+ * @type {import("expo/config-plugins").ConfigPlugin}
+ */
+const withAndroidPackageVisibilityFiltering = (config) => {
   return withAndroidManifest(config, (config) => {
-    // Check if the manifest already contains the intent filter
-    if (
-      Array.isArray(config.modResults.manifest.queries) &&
-      config.modResults.manifest.queries.some((query) => {
-        return (
-          query.package?.[0]?.$?.["android:name"] ===
-          "com.google.android.googlequicksearchbox"
-        );
-      })
-    ) {
-      return config;
-    }
-
     /**
      Appends the following to our AndroidManifest:
      <queries>
@@ -31,15 +23,11 @@ const withExpoSpeechRecognition /*: ConfigPlugin*/ = (
       ...(config.modResults.manifest.queries || []),
       {
         package: {
-          $: {
-            "android:name": "com.google.android.googlequicksearchbox",
-          },
+          $: { "android:name": "com.google.android.googlequicksearchbox" },
         },
         intent: {
           action: {
-            $: {
-              "android:name": "android.speech.RecognitionService",
-            },
+            $: { "android:name": "android.speech.RecognitionService" },
           },
         },
       },
@@ -48,4 +36,41 @@ const withExpoSpeechRecognition /*: ConfigPlugin*/ = (
   });
 };
 
-module.exports = withExpoSpeechRecognition;
+/**
+ * @type {import("expo/config-plugins").ConfigPlugin<{microphonePermission?: string; speechRecognitionPermission?: string;}>}
+ */
+const withExpoSpeechRecognition = (config, props) => {
+  if (!config.ios) {
+    config.ios = {};
+  }
+
+  if (!config.ios.infoPlist) {
+    config.ios.infoPlist = {};
+  }
+
+  config.modResults.NSSpeechRecognitionUsageDescription =
+    props.speechRecognitionPermission ||
+    config.ios.infoPlist.NSSpeechRecognitionUsageDescription ||
+    "Allow $(PRODUCT_NAME) to use speech recognition.";
+
+  config.ios.infoPlist.NSMicrophoneUsageDescription =
+    props.microphonePermission ||
+    config.ios.infoPlist.NSMicrophoneUsageDescription ||
+    "Allow $(PRODUCT_NAME) to use the microphone.";
+
+  return withPlugins(config, [
+    // Android permissions
+    [
+      AndroidConfig.Permissions.withPermissions,
+      ["android.permission.RECORD_AUDIO"],
+    ],
+    // Android package visibility filtering (for the Google App "com.google.android.googlequicksearchbox")
+    withAndroidPackageVisibilityFiltering,
+  ]);
+};
+
+module.exports = createRunOncePlugin(
+  withExpoSpeechRecognition,
+  "expo-speech-recognition",
+  "1.0.0",
+);
