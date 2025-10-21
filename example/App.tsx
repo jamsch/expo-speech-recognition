eae08c27576a5d614974dfeba5642e02b4f3b465
jamsch <12927717+jamsch@users.noreply.github.com>
2025-10-21 20:23:59 +1300

    feat: expo sdk 53 -> 54

--- example/App.tsx
+++ example/App.tsx
@@ -38,7 +38,6 @@ import {
   SmallButton,
 } from "./components/Buttons";
 import { StatusBar } from "expo-status-bar";
-import { Audio } from "expo-av";
 import { useAssets } from "expo-asset";
 import * as FileSystem from "expo-file-system";
 import {
@@ -985,8 +984,6 @@ function OtherSettings(props: {
 
       <WebSpeechAPIDemo />
 
-      <RecordUsingExpoAvDemo />
-
       <TranscribeLocalAudioFile />
 
       <TranscribeRemoteAudioFile
@@ -1209,106 +1206,6 @@ function WebSpeechAPIDemo() {
           {transcription?.transcript || "Transcripts goes here"}
         </Text>
       </ScrollView>
-    </View>
-  );
-}
-
-function RecordUsingExpoAvDemo() {
-  const [isRecording, setIsRecording] = useState(false);
-  const recordingRef = useRef<Audio.Recording | null>(null);
-  const [recordingUri, setRecordingUri] = useState<string | null>(null);
-
-  const handleStart = async () => {
-    setIsRecording(true);
-    try {
-      await Audio.setAudioModeAsync({
-        allowsRecordingIOS: true,
-        playsInSilentModeIOS: true,
-      });
-      const { recording } = await Audio.Recording.createAsync({
-        isMeteringEnabled: true,
-        android: {
-          bitRate: 32000,
-          extension: ".m4a",
-          outputFormat: AndroidOutputFormat.MPEG_4,
-          audioEncoder: AndroidAudioEncoder.AAC,
-          numberOfChannels: 1,
-          sampleRate: 16000,
-        },
-        ios: {
-          ...Audio.RecordingOptionsPresets.HIGH_QUALITY.ios,
-          numberOfChannels: 1,
-          bitRate: 16000,
-          extension: ".wav",
-          outputFormat: IOSOutputFormat.LINEARPCM,
-        },
-        web: {
-          mimeType: "audio/wav",
-          bitsPerSecond: 128000,
-        },
-      });
-
-      recordingRef.current = recording;
-    } catch (e) {
-      console.log("Error starting recording", e);
-    }
-  };
-
-  const handleStop = async () => {
-    setIsRecording(false);
-    const recording = recordingRef.current;
-    if (!recording) {
-      return;
-    }
-    await recording.stopAndUnloadAsync();
-    const uri = recording.getURI();
-    setRecordingUri(uri);
-  };
-
-  return (
-    <View style={styles.card}>
-      <Text style={[styles.text, styles.mb2]}>Record using Expo AV</Text>
-
-      <View style={styles.row}>
-        {!isRecording ? (
-          <BigButton
-            title="Start Recording"
-            color="#539bf5"
-            onPress={handleStart}
-          />
-        ) : (
-          <BigButton
-            title="Stop Recording"
-            color="#7C90DB"
-            onPress={handleStop}
-          />
-        )}
-      </View>
-
-      {recordingUri && <AudioPlayer source={recordingUri} />}
-
-      {recordingUri && (
-        <BigButton
-          title="Transcribe the recording"
-          color="#539bf5"
-          onPress={() => {
-            console.log("Transcribing recording", recordingUri);
-            ExpoSpeechRecognitionModule.start({
-              lang: "en-US",
-              interimResults: true,
-              // Switch to true for faster transcription
-              // (Make sure you downloaded the offline model first)
-              requiresOnDeviceRecognition: false,
-              audioSource: {
-                uri: recordingUri,
-                audioChannels: 1,
-                audioEncoding: AudioEncodingAndroid.ENCODING_MP3,
-                sampleRate: 16000,
-              },
-            });
-          }}
-        />
-      )}
     </View>
   );
 }
